565e7e3b60c56bae2e1f8af8fec5f735cde03dc1
David-Lundmark <david.lundmarkbredband.net>
2016-11-08 09:22:49 +0100

    Init

---
+++ Lagersystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lagersystem.Startup))]
+namespace Lagersystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
